f0e0af57f9e5f40554fc84cb81e5f3571acf2a7e
scarytom <none@none>
2010-08-01 11:11:41 +0100

    add support for codeplex

--- src/org/netmelody/osnamer/shared/ProjectHostName.java
+++ src/org/netmelody/osnamer/shared/ProjectHostName.java
@@ -4,7 +4,7 @@ public enum ProjectHostName {
     GITHUB("<img src=\"http://github.com/images/modules/header/logov3.png\" alt=\"GitHub\""),
     BITBUCKET("<img src=\"http://bitbucket-assets.s3.amazonaws.com/img/logo_myriad.png\" alt=\"bitbucket\""),
     SOURCEFORGE("<img src=\"http://mirrorbrain.org/static/images/misc/sourceforge-logo.png\" alt=\"sourceforge\""),
-    CODEPLEX("<img src=\"http://i1.codeplex.com/Images/v17027/logo-home.png\" alt=\"sourceforge\"");
+    CODEPLEX("<img src=\"http://i1.codeplex.com/Images/v17027/logo-home.png\" alt=\"CodePlex\" style=\"background-color: lightgray;\"");
 
     private final String htmlDesc;
 
